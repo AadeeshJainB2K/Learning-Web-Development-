73e47b27a55c6212dbc6e5ad20821a424b36e938
AadeeshJainB2K <aadeeshjain15@gmail.com>
2024-03-14 18:42:29 +0530

    Completed Question 1

--- Web Development/JavaScript/Part - 1 (Variables And Data Types)/Question 1,1/Question 1,1.js
+++ Web Development/JavaScript/Part - 1 (Variables And Data Types)/Question 1,1/Question 1,1.js
@@ -1,1 +1,12 @@
-const porduct = {};
+const product = {
+  productName: "BallPen",
+  productRating: 4,
+  numberOfRating: 7.002,
+  isDealOfTheDay: true,
+  productPrice: 285,
+  isOfeer: true,
+  offer: "5% off",
+  finalPrice: 270,
+};
+
+console.log(product);

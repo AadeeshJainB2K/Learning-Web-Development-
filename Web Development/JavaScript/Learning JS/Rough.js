d1a5e4e89bd422b92741718fc1b38c391601ef88
AadeeshJainB2K <aadeeshjain15@gmail.com>
2024-03-15 15:57:27 +0530

    +1

---
+++ Web Development/JavaScript/Learning JS/Rough.js
@@ -0,0 +1,17 @@
+let a = 5;
+let b = 2;
+console.log("a = 5 , b= 2");
+console.log("a+b = ", a + b);
+console.log("a-b = ", a - b);
+console.log("a*b = ", a * b);
+console.log("a/b = ", a / b);
+console.log("a%b = ", a % b);
+console.log("a**b = ", a ** b);
+// Increament & Decrement
+console.log(" a + 1 = a++ =", a++);
+console.log(a);
+console.log(" a - 1 = a-- =", a--);
+console.log(" a + 1 = ++a =", ++a);
+console.log(" a + 1 = --a =", a--);
+console.log(" a + 1 = a++ =", a++);
+// Assignment OPREATERS
--- Web Development/JavaScript/Learning JS/index.js
+++
@@ -1,7 +0,0 @@
-let Name = "Aadeesh Jain";
-const Class = "9th";
-Class = "10th";
-
-Age = "14";
-console.log(Name, Class);
-console.log(Age);

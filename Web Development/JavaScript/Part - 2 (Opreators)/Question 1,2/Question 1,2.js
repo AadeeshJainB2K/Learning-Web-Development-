4eb7e7f5166b155fd018ca43eb170705dc61de62
AadeeshJainB2K <aadeeshjain15@gmail.com>
2024-03-16 20:22:42 +0530

    +1

--- Web Development/JavaScript/Part - 2 (Opreators)/Question 1,2/Question 1,2.js
+++ Web Development/JavaScript/Part - 2 (Opreators)/Question 1,2/Question 1,2.js
@@ -1,12 +1,9 @@
-const product = {
-  productName: "BallPen",
-  productRating: 4,
-  numberOfRating: 7.002,
-  isDealOfTheDay: true,
-  productPrice: 285,
-  isOfeer: true,
-  offer: "5% off",
-  finalPrice: 270,
-};
-
-console.log(product);
+let number = prompt("Enter a Munber Here To Check If It Is A Multiple Of 5");
+let finalNumber = number % 5;
+if (finalNumber === 0) {
+  console.log(number, " is a multiple of 5");
+} else if (finalNumber === !0) {
+  console.log(number, "is not a multiple of 5");
+} else {
+  throw ("!!! Error !!! ", "The Number You Have Enetrd Is Not A Valid Number");
+}

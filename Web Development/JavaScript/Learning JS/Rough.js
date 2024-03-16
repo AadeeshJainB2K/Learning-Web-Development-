22619006ef14458ce17e8e9cfc010107bb275f8f
AadeeshJainB2K <aadeeshjain15@gmail.com>
2024-03-16 20:05:44 +0530

    +1

--- Web Development/JavaScript/Learning JS/Rough.js
+++ Web Development/JavaScript/Learning JS/Rough.js
@@ -1,4 +1,13 @@
-let age = 18;
-if (age === 18) {
-  console.log("Eligible for Voter ID");
+let number = "ahjsfbcksgjda gkas dc";
+let b = number % 2;
+if (b === 0) {
+  console.log("Its A Even Number");
+} else if (b === 1) {
+  console.log("Its a Odd Number");
+} else {
+  console.log("Invalid Number");
 }
+
+let age = 9;
+let result = age >= 18 ? "Can Drive With A lisence" : "Can't Drive ";
+console.log(result);

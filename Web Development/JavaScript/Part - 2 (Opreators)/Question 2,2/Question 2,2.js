dd79682470319770031f8f03d0c6fe632a329603
AadeeshJainB2K <aadeeshjain15@gmail.com>
2024-03-16 20:39:56 +0530

    +1

--- Web Development/JavaScript/Part - 2 (Opreators)/Question 2,2/Question 2,2.js
+++ Web Development/JavaScript/Part - 2 (Opreators)/Question 2,2/Question 2,2.js
@@ -1,12 +1,14 @@
-const product = {
-  productName: "BallPen",
-  productRating: 4,
-  numberOfRating: 7.002,
-  isDealOfTheDay: true,
-  productPrice: 285,
-  isOfeer: true,
-  offer: "5% off",
-  finalPrice: 270,
-};
-
-console.log(product);
+let Marks = prompt("Enter Your Marks , So I Can Tell You Your Grades");
+if (Marks >= 80 && Marks <= 100) {
+  console.log("Your Greade is A");
+} else if (Marks >= 70 && Marks < 89) {
+  console.log("Your Greade is B");
+} else if (Marks >= 60 && Marks < 69) {
+  console.log("Your Greade is C");
+} else if (Marks >= 50 && Marks < 59) {
+  console.log("Your Greade is D");
+} else if (Marks >= 0 && Marks < 49) {
+  console.log("Your Greade is E");
+} else {
+  console.log("Enter A Valid Number");
+}

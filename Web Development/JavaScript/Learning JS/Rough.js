f0e99aa5cebd429217a2ae96a9f29e83b2dfe961
AadeeshJainB2K <aadeeshjain15@gmail.com>
2024-03-17 07:57:28 +0530

    Created Calc in rough.js

--- Web Development/JavaScript/Learning JS/Rough.js
+++ Web Development/JavaScript/Learning JS/Rough.js
@@ -1,13 +1,18 @@
-let number = "ahjsfbcksgjda gkas dc";
-let b = number % 2;
-if (b === 0) {
-  console.log("Its A Even Number");
-} else if (b === 1) {
-  console.log("Its a Odd Number");
-} else {
-  console.log("Invalid Number");
+// Defining The Varialbles
+let a;
+let b;
+let c;
+// Taking Input
+a = parseFloat(prompt("Enter First Number"));
+b = prompt("Enter The Opreation");
+c = parseFloat(prompt("Enter First Number"));
+// creating Logic
+if (b == "+") {
+  console.log(a + c);
+} else if (b == "-") {
+  console.log(a - c);
+} else if (b == "*") {
+  console.log(a * c);
+} else if (b == "/") {
+  console.log(a / c);
 }
-
-let age = 9;
-let result = age >= 18 ? "Can Drive With A lisence" : "Can't Drive ";
-console.log(result);

164fb47b7a2d68bd21ceaa79a7cba332d34f0db0
AadeeshJainB2K <aadeeshjain15@gmail.com>
2024-03-17 08:29:37 +0530

    +1

---
+++ Web Development/JavaScript/CalculateTheSumOfConsecutiveNumbers/CalculateTheSumOfConsecutiveNumbers.js
@@ -0,0 +1,7 @@
+let number = parseFloat(
+  prompt(
+    "Enter A Number To Calculate The Sum Of 1 to The Number You Have Entered"
+  )
+);
+
+let Result;
--- Web Development/JavaScript/Learning JS/Rough.js
+++ Web Development/JavaScript/Learning JS/Rough.js
@@ -1,18 +1,4 @@
-// Defining The Varialbles
-let a;
-let b;
-let c;
-// Taking Input
-a = parseFloat(prompt("Enter First Number"));
-b = prompt("Enter The Opreation");
-c = parseFloat(prompt("Enter First Number"));
-// creating Logic
-if (b == "+") {
-  console.log(a + c);
-} else if (b == "-") {
-  console.log(a - c);
-} else if (b == "*") {
-  console.log(a * c);
-} else if (b == "/") {
-  console.log(a / c);
+let i = 0;
+for (i = 1; i <= 1; i++) {
+  console.log("*");
 }

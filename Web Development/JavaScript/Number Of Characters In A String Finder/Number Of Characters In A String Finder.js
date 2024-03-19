af00764fc4f6df0c304429917d776b6711f3c1d1
AadeeshJainB2K <aadeeshjain15@gmail.com>
2024-03-19 17:08:42 +0530

    Completed Number Of Characters In A String Finder

--- Web Development/JavaScript/Number Of Characters In A String Finder/Number Of Characters In A String Finder.js
+++ Web Development/JavaScript/Number Of Characters In A String Finder/Number Of Characters In A String Finder.js
@@ -1,5 +1,13 @@
-let str = Aa;
-let result = 0;
-for (str of result) {
-  console.log(result);
+let str = prompt(
+  "Enter A Word Or A Paragraph To Check Its Character Length In The Console"
+);
+
+let result;
+
+let count = 0;
+
+for (result of str) {
+  count++;
 }
+
+console.log(count);

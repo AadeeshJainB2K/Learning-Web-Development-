f02aa5938c79274123478ed68be503dc5e10aa70
AadeeshJainB2K <aadeeshjain15@gmail.com>
2024-03-21 21:53:07 +0530

    +1

--- Web Development/JavaScript/Part - 5 (Arrays)/Question 2,4/Question 1,5.js
+++ Web Development/JavaScript/Part - 5 (Arrays)/Question 2,4/Question 1,5.js
@@ -1,5 +1,4 @@
 let heros = ["Aadeesh Jain", "Shaktiman", "Spiderman", "Paw Petrol"];
-
-for (let i = 0; i <= heros.length - 1; i++) {
-  console.log(heros[i]);
+for (let i of heros) {
+  console.log(i.toUpperCase());
 }

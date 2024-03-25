adeabd1f8bf5453d9aa93bf4c17fb4b3b8147863
AadeeshJainB2K <aadeeshjain15@gmail.com>
2024-03-25 22:31:06 +0530

    Completed The Project

--- Web Development/JavaScript/Stone Paper Scissors/Stone Paper Scissors.js
+++ Web Development/JavaScript/Stone Paper Scissors/Stone Paper Scissors.js
@@ -12,6 +12,14 @@ let message = document.querySelector("#message");
 
 let btn = document.querySelector("#messageBox");
 
+let usersScorePara = document.querySelector("#UsersScore");
+
+let machinesScorePara = document.querySelector("#ComputersScore");
+
+usersScorePara.innerText = usersScore;
+
+machinesScorePara.innerText = machinesScore;
+
 let machinesChoiceGenerator = () => {
   let index = Math.floor(Math.random() * 3);
   return options[index];
@@ -29,21 +37,33 @@ usersChoice = availableChoices.forEach((choice) => {
     } else if (usersChoice === "stone" && machinesChoice === "scissors") {
       message.innerText = "You Won . Your Stone Beats Scissors";
       btn.setAttribute("class", "YouWon sparkleChange");
+      usersScore++;
+      usersScorePara.innerText = usersScore;
     } else if (usersChoice === "stone" && machinesChoice === "paper") {
       message.innerText = "You Lost . Paper Beats Your Rock";
       btn.setAttribute("class", "YouLost sparkleChange");
+      machinesScorePara.innerText = machinesScore;
+      machinesScore++;
     } else if (usersChoice === "scissors" && machinesChoice === "paper") {
       message.innerText = "You Won . Your Scissors Beats Paper";
       btn.setAttribute("class", "YouWon sparkleChange");
+      usersScore++;
+      usersScorePara.innerText = usersScore;
     } else if (usersChoice === "scissors" && machinesChoice === "stone") {
       message.innerText = "You Lost . Stone Beats Your Scissors";
       btn.setAttribute("class", "YouLost sparkleChange");
+      machinesScorePara.innerText = machinesScore;
+      machinesScore++;
     } else if (usersChoice === "paper" && machinesChoice === "stone") {
       message.innerText = "You Won . Your Paper Beats Stone";
       btn.setAttribute("class", "YouWon sparkleChange");
+      usersScore++;
+      usersScorePara.innerText = usersScore;
     } else if (usersChoice === "paper" && machinesChoice === "scissors") {
       message.innerText = "You Lost . Scissors Beats Your Paper";
       btn.setAttribute("class", "YouLost sparkleChange");
+      machinesScorePara.innerText = machinesScore;
+      machinesScore++;
     }
     return choice.getAttribute("id");
   });

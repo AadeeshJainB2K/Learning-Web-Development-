29b35ba9d88fc736fa3df9a4c5a8ed0fb6534293
AadeeshJainB2K <aadeeshjain15@gmail.com>
2024-03-25 22:45:34 +0530

    blabalablablablablablbalal

--- Web Development/JavaScript/Learning JS/Rough.js
+++ Web Development/JavaScript/Learning JS/Rough.js
@@ -1,15 +1,9 @@
-let button = document.createElement("button");
-button.innerText = "ClickMe!";
-button.style.backgroundColor = "red";
-button.style.color = "blue";
-let paragraph = document.querySelector("p");
-paragraph.prepend(button);
-button.addEventListener("click", () => {
-  console.log("you clicked");
-});
-button.addEventListener("click", () => {
-  console.log("you clicked by callback 2");
-});
-button.removeEventListener("click", () => {
-  console.log("you clicked by callback 2");
-});
+let student = {
+  firstName: "Aadeesh ",
+  lastName: "Jain",
+  standard: 9,
+  result: "88%",
+  printResult: () => {
+    console.log(student.result);
+  },
+};

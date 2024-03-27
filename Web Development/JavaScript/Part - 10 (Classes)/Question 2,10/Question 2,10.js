58a120d763e70d720eb7f8f416fc66c85dfd1bc3
AadeeshJainB2K <aadeeshjain15@gmail.com>
2024-03-27 20:13:47 +0530

    Completed Both Questions of classes

--- Web Development/JavaScript/Part - 10 (Classes)/Question 2,10/Question 2,10.js
+++ Web Development/JavaScript/Part - 10 (Classes)/Question 2,10/Question 2,10.js
@@ -1,6 +1,17 @@
-class user {}
-class admin {
+class user {
+  constructor(name, email) {
+    this.name = name;
+    this.email = email;
+  }
+}
+class admin extends user {
+  constructor(name, email) {
+    super(name, email);
+  }
   editData() {
     console.log("Now You Can Edit Data");
   }
 }
+let usr1 = new user("user1", "user1@mail.com");
+let usr2 = new user("user2", "user2@mail.com");
+let adm1 = new admin("adm1", "adm1@mail.com");

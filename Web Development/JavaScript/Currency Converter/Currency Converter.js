ac9bfdf5410bff4a3492044d066097b6d9c49dbb
AadeeshJainB2K <aadeeshjain15@gmail.com>
2024-04-04 21:20:31 +0530

    discontinueing the project

--- Web Development/JavaScript/Currency Converter/Currency Converter.js
+++ Web Development/JavaScript/Currency Converter/Currency Converter.js
@@ -1,1 +1,22 @@
-console.log(countryList);
+const currencyAPI =
+  "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/eur/jpy.json";
+
+currencyAPI_Data = async () => {
+  let rawData = await fetch(currencyAPI);
+  let usableData = await rawData.json();
+  return usableData;
+};
+
+let options = document.querySelector(".countries");
+
+for (selector of options) {
+  let Countries = selector.createElement("select");
+  Countries.innerText = "Br";
+  selector.append(countries);
+}
+
+for (i in countryList) {
+  let a = 0;
+  console.log(i, countryList[i]);
+  a++;
+}

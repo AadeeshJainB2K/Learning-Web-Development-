77987d6821ce73175d1a2580f59d49e9a575ab52
AadeeshJainB2K <aadeeshjain15@gmail.com>
2024-04-08 15:08:20 +0530

    completed the todo's list

--- Web Development/JavaScript/To-Do List/To-Do List.js
+++ Web Development/JavaScript/To-Do List/To-Do List.js
@@ -15,31 +15,26 @@ function addTask() {
   let mainLi = document.createElement("li");
   let listContainer = document.createElement("div");
   let listContainerFirst = document.createElement("div");
-  let uncheckedImg = document.createElement("img");
   let task = document.createElement("p");
   let crossSymbol = document.createElement("span");
   listContainerFirst.classList.add("listContainerFirst");
   mainLi.append(listContainerFirst);
   mainLi.classList.add("mainLi");
   task.innerText = input.value;
-  uncheckedImg.classList.add("uncheckedImg");
-  uncheckedImg.src = "unchecked.png";
   listContainer.classList.add("listContainer");
   listField.appendChild(mainLi);
   mainLi.appendChild(listContainer);
-  listContainerFirst.appendChild(uncheckedImg);
   listContainerFirst.appendChild(task);
   crossSymbol.innerHTML = "&#10005;";
   crossSymbol.classList.add("cross");
   listContainer.appendChild(crossSymbol);
-  input.value = "";
-  listContainer.addEventListener("click", (i) => {
-    if (i.target.tagName === "SPAN") {
+  listContainer.addEventListener("click", (event) => {
+    const target = event.target;
+    if (target.tagName === "SPAN") {
       mainLi.remove();
-    } else if (i.target.tagName === "LI") {
-      console.log("abcd");
     }
   });
+  input.value = "";
 }
 
 btn.addEventListener("click", () => {

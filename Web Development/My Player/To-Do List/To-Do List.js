799e14e8c776c438a02bb4c8e5625d67e017bb1d
AadeeshJainB2K <aadeeshjain15@gmail.com>
2024-04-08 13:26:29 +0530

    halfway completed the js

--- Web Development/My Player/To-Do List/To-Do List.js
+++ Web Development/My Player/To-Do List/To-Do List.js
@@ -12,18 +12,34 @@ input.addEventListener("keypress", function (event) {
 });
 
 function addTask() {
-  let li = document.createElement("li");
-  li.innerText = input.value;
-  listField.appendChild(li);
-  let crossIcon = document.createElement("span");
-  crossIcon.innerText = "⨯";
-  li.appendChild(crossIcon);
+  let mainLi = document.createElement("li");
+  let listContainer = document.createElement("div");
+  let listContainerFirst = document.createElement("div");
+  let uncheckedImg = document.createElement("img");
+  let task = document.createElement("p");
+  let crossSymbol = document.createElement("span");
+  listContainerFirst.classList.add("listContainerFirst");
+  mainLi.append(listContainerFirst);
+  task.innerText = input.value;
+  uncheckedImg.classList.add("uncheckedImg");
+  uncheckedImg.src = "unchecked.png";
+  listContainer.classList.add("listContainer");
+  listField.appendChild(mainLi);
+  mainLi.appendChild(listContainer);
+  listContainerFirst.appendChild(uncheckedImg);
+  listContainerFirst.appendChild(task);
+  crossSymbol.innerHTML = "&#10005;";
+  crossSymbol.classList.add("cross");
+  listContainer.appendChild(crossSymbol);
   input.value = "";
 }
 
 btn.addEventListener("click", () => {
-  if (input.value === " ") {
+  if (input.value === "") {
     alert("You Are Supposed To Write Something !");
+  } else if (input.value === " ") {
+    alert("You Are Supposed To Write Something !");
+  } else {
+    addTask();
   }
-  addTask();
 });

b0a2ae8d00d52ac4960a90855baf3a4a241040df
AadeeshJainB2K <aadeeshjain15@gmail.com>
2024-04-09 16:53:02 +0530

    +1

--- Web Development/Learning React/to-dos-list/src/Components/Header.js
+++ Web Development/Learning React/to-dos-list/src/Components/Header.js
@@ -28,17 +28,6 @@ export default function Header(props) {
               <a className="nav-link">About</a>
             </li>
           </ul>
-          <form className="d-flex">
-            <input
-              className="form-control me-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
-            <button className="btn btn-outline-success" type="submit">
-              Search
-            </button>
-          </form>
         </div>
       </div>
     </nav>

5eeab4d18bf89d0e9ab96fabd9cf492b01b3c475
AadeeshJainB2K <aadeeshjain15@gmail.com>
2024-04-21 14:49:44 +0530

    API NOT WORKING , MISSION ABORT

--- Web Development/JavaScript/Currency Converter/Currency Converter.js
+++ Web Development/JavaScript/Currency Converter/Currency Converter.js
@@ -1,22 +1,5 @@
-const currencyAPI =
-  "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/eur/jpy.json";
-
-currencyAPI_Data = async () => {
-  let rawData = await fetch(currencyAPI);
-  let usableData = await rawData.json();
-  return usableData;
-};
-
-let options = document.querySelector(".countries");
-
-for (selector of options) {
-  let Countries = selector.createElement("select");
-  Countries.innerText = "Br";
-  selector.append(countries);
-}
-
-for (i in countryList) {
-  let a = 0;
-  console.log(i, countryList[i]);
-  a++;
-}
+const currencyAPI = fetchJSON(
+  `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2024-03-06/v1/currencies/usd`
+);
+rate = json[usd][inr];
+console.log(rate);

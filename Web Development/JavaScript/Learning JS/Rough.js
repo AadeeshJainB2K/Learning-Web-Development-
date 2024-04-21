6ea81aa1664457b288f4d1491d129649293b9ed4
AadeeshJainB2K <aadeeshjain15@gmail.com>
2024-04-21 13:57:29 +0530

    +1

--- Web Development/JavaScript/Learning JS/Rough.js
+++ Web Development/JavaScript/Learning JS/Rough.js
@@ -1,15 +1,4 @@
-document.body.addEventListener("mousemove", (evt) => {
-  const mouseX = evt.clientX;
-  const mouseY = evt.clientY;
-
-  gsap.set(".cursor", {
-    x: mouseX,
-    y: mouseY,
-  });
-
-  gsap.to(".shape", {
-    x: mouseX,
-    y: mouseY,
-    stagger: -0.1,
-  });
+const cron = require("node-cron");
+cron.schedule("* * * * *", () => {
+  console.log("Worked");
 });

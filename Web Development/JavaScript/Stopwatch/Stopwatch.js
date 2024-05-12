c67e0b5361c155f9e1b6d4e43e7aa1f404ac1841
AadeeshJainB2K <aadeeshjain15@gmail.com>
2024-05-12 09:02:56 +0530

    completed functoinnality

--- Web Development/JavaScript/Stopwatch/Stopwatch.js
+++ Web Development/JavaScript/Stopwatch/Stopwatch.js
@@ -6,16 +6,39 @@ let startBtn = document.querySelector("#startTimer");
 
 let resetBtn = document.querySelector("#resetTimer");
 
-timerDisplay.innerText = `${hours} : ${minutes} : ${seconds} : ${miliseconds}`;
+timerDisplay.innerText = `${padTime(hours)} : ${padTime(minutes)} : ${padTime(
+  seconds
+)} : ${padTime(miliseconds)}`;
+
+function padTime(time) {
+  return time < 10 ? `0${time}` : time;
+}
 
 function functionality() {
   setInterval(() => {
     miliseconds++;
-    timerDisplay.innerText = `${hours} : ${minutes} : ${seconds} : ${miliseconds}`;
+    timerDisplay.innerText = `${padTime(hours)} : ${padTime(
+      minutes
+    )} : ${padTime(seconds)} : ${padTime(miliseconds)}`;
+
     if (miliseconds >= 99) {
       seconds++;
       miliseconds = 0;
     }
+    if (seconds >= 60) {
+      minutes++;
+      seconds = 0;
+    }
+    if (minutes >= 60) {
+      hours++;
+      minutes = 0;
+    }
   }, 1);
 }
-resetBtn.click(functionality());
+
+startBtn.addEventListener("click", functionality());
+
+resetBtn.addEventListener(
+  "click",
+  (timerDisplay.innerText = `00 : 00 : 00: 00`)
+);

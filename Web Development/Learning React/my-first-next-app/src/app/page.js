2bab0d38db96ddaa2292d0db5f356216d36c41e4
AadeeshJainB2K <aadeeshjain15@gmail.com>
2024-06-05 18:16:06 +0530

    +2

--- Web Development/Learning React/my-first-next-app/src/app/page.js
+++ Web Development/Learning React/my-first-next-app/src/app/page.js
@@ -1,25 +1,13 @@
 "use client";
+
 import React from "react";
 
+import Header from "../Components/Header";
+
 const page = () => {
-  let marks = 96;
-  const changeMarks = () => {
-    console.log(marks);
-    marks = 33;
-    console.log(marks);
-  };
   return (
     <>
-      <div className="font-bold text-xl text-red-600">
-        My marks in cs were {marks}
-      </div>
-      <button
-        onClick={() => {
-          marks = 33;
-        }}
-      >
-        Update
-      </button>
+      <Header />
     </>
   );
 };

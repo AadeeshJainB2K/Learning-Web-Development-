f61db4493d30a299d23d45500ce893cd80acc577
AadeeshJainB2K <aadeeshjain15@gmail.com>
2024-06-11 22:48:09 +0530

    Started a new chat bot project

---
+++ Web Development/Learning React/ai-chat-bot/app/Components/Main.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+
+const Main = () => {
+  return (
+    <>
+      <main>
+        <section id="OptionsSection">
+          <p id="Fact"></p>
+        </section>
+        <section id="PickBtnSection">
+          <button id="btn">
+            <b>Get a Random Cat Fact</b>
+          </button>
+        </section>
+      </main>
+    </>
+  );
+};
+
+export default Main;
---
+++ Web Development/Learning React/ai-chat-bot/app/page.jsx
@@ -0,0 +1,18 @@
+"use client";
+
+import React from "react";
+import Header from "./Components/Header";
+import Main from "./Components/Main";
+import Footer from "./Components/Footer";
+
+const page = () => {
+  return (
+    <>
+      <Header></Header>
+      <Main></Main>
+      <Footer></Footer>
+    </>
+  );
+};
+
+export default page;

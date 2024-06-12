c048b504149d6483ae2500f3fc02129c52968b84
AadeeshJainB2K <aadeeshjain15@gmail.com>
2024-06-12 20:49:25 +0530

    Linked a web css

--- Web Development/Learning React/gemini-ai-chatbot/app/layout.jsx
+++ Web Development/Learning React/gemini-ai-chatbot/app/layout.jsx
@@ -11,6 +11,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
+        />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
---
+++ Web Development/Learning React/gemini-ai-chatbot/app/page.jsx
@@ -0,0 +1,15 @@
+"use client";
+import React from "react";
+import Main from "./Components/Main";
+import Header from "./Components/Header";
+
+const page = () => {
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  );
+};
+
+export default page;

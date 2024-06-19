7e3d5b35e9c167c75f2385d9be084fd44d1b212d
AadeeshJainB2K <aadeeshjain15@gmail.com>
2024-06-19 14:07:39 +0530

    Started A new project todos list

---
+++ Web Development/Learning React/Next JS/to-dos-list/app/page.jsx
@@ -0,0 +1,13 @@
+"use-client";
+import MainApp from "@/Components/MainApp";
+import React from "react";
+
+const page = () => {
+  return (
+    <>
+      <MainApp />
+    </>
+  );
+};
+
+export default page;

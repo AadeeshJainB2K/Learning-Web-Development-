382f3a7f773a7f7a547a17ba0060615fb1b84731
AadeeshJainB2K <aadeeshjain15@gmail.com>
2024-11-21 14:07:23 +0530

    +1

--- Web Development/Learning React/Next JS/learning-ts/src/app/page.tsx
+++ Web Development/Learning React/Next JS/learning-ts/src/app/page.tsx
@@ -1,28 +1,12 @@
 "use client";
 import React from "react";
 
-interface User {
-  Name: string;
-  Phone_Number: number;
-  Email: string;
-  Password: string;
-  Gender?: string;
-}
 
-interface Admin extends User {
-  Admin: boolean;
-}
-
-const a = (obj: User) => {
-  return obj.Email;
-};
-
-const b = a({ Email: "aadeeshjain@gmail.com" });
 
 const page = () => {
   return (
     <>
-      <div>{b}</div>
+      <div>{}</div>
     </>
   );
 };

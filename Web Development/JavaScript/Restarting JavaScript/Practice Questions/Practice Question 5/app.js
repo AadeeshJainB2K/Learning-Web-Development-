ec8ad3c793ef6423d2c246b03eea17dbbcaa4046
Aadeesh Jain <aadeeshjain15@gmail.com>
2025-11-30 13:15:28 +0530

    +1

--- Web Development/JavaScript/Restarting JavaScript/Practice Questions/Practice Question 5/app.js
+++ Web Development/JavaScript/Restarting JavaScript/Practice Questions/Practice Question 5/app.js
@@ -1,6 +1,7 @@
 n = prompt("enter a number get sum of numbers till n : ");
 sum = 0;
-for (let i = 0; i > n; i++) {
+for (let i = 0; i <= n; i++) {
   sum += i;
-  console.log(sum);
 }
+console.log(sum);
+alert(sum);

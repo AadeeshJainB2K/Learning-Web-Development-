e33c7b520cceb409ab2c707e16bfee19165a4e66
Aadeesh Jain <aadeeshjain15@gmail.com>
2025-11-30 17:02:20 +0530

    +1

--- Web Development/JavaScript/Restarting JavaScript/Rough/app.js
+++ Web Development/JavaScript/Restarting JavaScript/Rough/app.js
@@ -1,5 +1,6 @@
 let arr = [1, 2, 3, 4, 5, 6, 6, 7, 8, 9];
-arr.forEach(function print(a, b, c) {
-  console.log(a ** a);
-  console.log(b, c);
+
+newArr = arr.map((a) => {
+  return a ** 2;
 });
+console.log(newArr);
